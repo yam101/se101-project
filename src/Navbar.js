5ed914c7f370bcb19518a92c88517b26b587b2fb
jessicaxu0605 <jessicaxu0605@gmail.com>
2023-11-14 13:38:41 -0500

    update Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,28 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import Button from '@mui/material/Button';
+
 
 function Navbar() {
     return (
         <div className="navbar">
             <ul className='pagelinks'>
                 <li>
-                    <Link to='/' className='navlink'>Home</Link>
+                    <Button variant="contained"><Link to='/' className='navlink'>Home</Link></Button> 
                 </li>
                 <li>
-                    <Link to='/' className='navlink'>Ur mom</Link>
+                    <Button variant="contained"><Link to='/' className='navlink'>Ur mom</Link></Button>
                 </li>
             </ul>
             <ul className='reglinks'>
                 <li>
-                    <Link to='/signup' className='navlink'>Sign Up</Link>
+                    <Button variant="contained"><Link to='/signup' className='navlink'>Sign Up</Link></Button>
                 </li>
                 <li>
-                    <Link to='/login' className='navlink'>Login</Link>
+                    <Button variant="contained"><Link to='/login' className='navlink'>Login</Link></Button> 
                 </li>
                 <li>
-                    <Link to='/profile' className='navlink'>Profile</Link>
+                    <Button variant="contained"><Link to='/profile' className='navlink'>Profile</Link></Button>
                 </li>
             </ul>
         </div>
