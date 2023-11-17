6ac27b91aa6b615842b0d62a2df1ba93e01b81d0
Yasmin Motahhary <yasminmotahhary@gmail.com>
2023-11-17 17:45:34 -0500

    edited stuff

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import ScrollTop from './ScrollTop';
 import Homepage from './Homepage';
 import Loginpage from './Loginpage';
 import Profile from './Profile';
-import ButtonAppBar from './Navbar_2';
+import Navbar from './Navbar';
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp';
+import Landing from './Landing';
 
 function App() {
   return (
     <div className="App">
-      <ButtonAppBar />
+      <Navbar />
       <ScrollTop /> {/*listens for change in route; if detected, scroll to top of page; does not render any HTML*/}
       <div className='spacer'></div>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Landing />} />
+        <Route path='/home' element={<Homepage />} />
         <Route path='/login' element={<Loginpage />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/signup' element={<SignUp />} />
