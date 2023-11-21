356d9f8c4853745d626dc6752541f1040eb3788e
Ellie Liang <e32liang@uwaterloo.ca>
2023-11-21 13:47:36 -0500

    Made changes to Profile

--- src/ProfileClassesTable.js
+++ src/ProfileClassesTable.js
@@ -8,9 +8,10 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
+// unfinished table!!!
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+    { id: 'course', label: 'Course', minWidth: 170 },
+    { id: 'code', label: 'Course Code', minWidth: 100 },
     {
       id: 'population',
       label: 'Population',
@@ -34,27 +35,27 @@ const columns = [
     },
   ];
   
-  function createData(name, code, population, size) {
+  function createData(course, code, population, size) {
     const density = population / size;
-    return { name, code, population, size, density };
+    return { course, code, population, size, density };
   }
   
   const rows = [
     createData('India', 'IN', 1324171354, 3287263),
     createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
+    // createData('Italy', 'IT', 60483973, 301340),
+    // createData('United States', 'US', 327167434, 9833520),
+    // createData('Canada', 'CA', 37602103, 9984670),
+    // createData('Australia', 'AU', 25475400, 7692024),
+    // createData('Germany', 'DE', 83019200, 357578),
+    // createData('Ireland', 'IE', 4857000, 70273),
+    // createData('Mexico', 'MX', 126577691, 1972550),
+    // createData('Japan', 'JP', 126317000, 377973),
+    // createData('France', 'FR', 67022000, 640679),
+    // createData('United Kingdom', 'GB', 67545757, 242495),
+    // createData('Russia', 'RU', 146793744, 17098246),
+    // createData('Nigeria', 'NG', 200962417, 923768),
+    // createData('Brazil', 'BR', 210147125, 8515767),
   ];
 
 function ProfileClassesTable(){
@@ -71,7 +72,7 @@ function ProfileClassesTable(){
   };
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+    <Paper sx={{ width: '100%', overflow: 'hidden', m: 0 }}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
