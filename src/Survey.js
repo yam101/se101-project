433041c63666c5b4ab3f311fe71f176c24953c69
Yasmin Motahhary <yasminmotahhary@gmail.com>
2023-11-28 18:53:12 -0500

    fixed console errors and adjusted ui

--- src/Survey.js
+++ src/Survey.js
@@ -3,25 +3,14 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import cover from "./images/background.png";
-import { GoogleSpreadsheet } from "google-spreadsheet";
 import Slider from './Slider';
 import Stack from '@mui/material/Stack';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-import { ScatterChart } from "@mui/x-charts/ScatterChart";
 import Card from '@mui/material/Card';
 
 
 const font = "'Poppins', sans-serif";
-
-const data = [
-    { x: 100, y: 200, id: 1 },
-    { x: 120, y: 100, id: 2 },
-    { x: 170, y: 300, id: 3 },
-    { x: 140, y: 250, id: 4 },
-    { x: 150, y: 400, id: 5 },
-    { x: 110, y: 280, id: 6 },
-  ];
 
 const theme = createTheme({
     typography: {
@@ -48,7 +37,7 @@ function Survey() {
             [name]: value,
         }));
     };
-    const handleSubmit = async() => {
+    const handleSubmit = async () => {
         console.log(sliderValues);
 
         const options = {
@@ -60,17 +49,17 @@ function Survey() {
                 q2: sliderValues['q2'],
                 q3: sliderValues['q3'],
                 q4: sliderValues['q4'],
-                
+
             })
-          }
-            const response = await fetch('http://localhost:3600/survey', options);
-            const result = await response.json();
-            console.log(result);
+        }
+        const response = await fetch('http://localhost:3600/survey', options);
+        const result = await response.json();
+        console.log(result);
     };
 
 
     return (
-        <> 
+        <>
             <div style={{
                 width: '100%', top: 'calc(0vh-100px)', height: '100%', backgroundImage: `url(${cover})`,
                 backgroundSize: 'cover', backgroundRepeat: 'no-repeat', position: 'fixed',
@@ -79,61 +68,56 @@ function Survey() {
             </div >
 
             <ThemeProvider theme={theme}>
-            <Stack spacing={2} direction="column" sx={{ pr:'200px', pl:'200px' }} alignItems="center">
+                <Stack spacing={2} direction="column" sx={{ pr: '250px', pl: '250px', pt: '100px' }} alignItems="center">
+                    <Typography variant="h4" gutterBottom>
+                        Academic Survey
+                    </Typography>
+                    <Box >
+                        <Typography variant="h6" gutterBottom>
+                            On a scale of 1 - 10, how often do you attend lectures for this class?
+                        </Typography>
+                    </Box>
+                    <Slider name='q1' onChange={handleSliderChange} />
+                    <Box >
+                        <div className='spacer'></div>
+                        <Typography variant="h6" gutterBottom>
+                            On a scale of 1 - 10, do you think these lectures have made a positive contribution to your academic success?
+                        </Typography>
+                    </Box>
+                    <Slider name='q2' onChange={handleSliderChange} />
 
-            <Box sx={{ zIndex: '2' }}>
-                <div class='spacer'></div>
-                <Typography variant="h6" gutterBottom>
-                On a scale of 1 - 10, how often do you attend lectures for this class?
+                    <Box>
+                        <div className='spacer'></div>
+                        <Typography variant="h6" gutterBottom>
+                            On a scale of 1 - 10, how would you rate the overall importance of attending lectures in your academic journey?
+                        </Typography>
+                    </Box>
 
-                </Typography>
-            </Box>
-            <Slider name='q1' onChange={handleSliderChange}/>
-
-
-            <Box sx={{ zIndex: '2' }}>
-                <div class='spacer'></div>
-                <Typography variant="h6" gutterBottom>
-                On a scale of 1 - 10, do you think these lectures have made a positive contribution to your academic success?
-
-                </Typography>
-            </Box>
-            <Slider name='q2' onChange={handleSliderChange}/>
-            
-            <Box sx={{ zIndex: '2' }}>
-                <div class='spacer'></div>
-                <Typography variant="h6" gutterBottom>
-                On a scale of 1 - 10, how would you rate the overall importance of attending lectures in your academic journey?
-
-                </Typography>
-            </Box>
-
-            <Slider name='q3' onChange={handleSliderChange}/>
+                    <Slider name='q3' onChange={handleSliderChange} />
 
 
-            <Box sx={{ zIndex: '2' }}>
-                <div class='spacer'></div>
-                <Typography variant="h6" gutterBottom>
-                On a scale of 1 to 10, to what extent do you believe that missing lectures has negatively impacted your academic performance?
+                    <Box>
+                        <div className='spacer'></div>
+                        <Typography variant="h6" gutterBottom>
+                            On a scale of 1 to 10, to what extent do you believe that missing lectures has negatively impacted your academic performance?
+                        </Typography>
+                    </Box>
+                    <Slider name='q4' onChange={handleSliderChange} />
 
-                </Typography>
-            </Box>
-            <Slider name='q4' onChange={handleSliderChange}/>
+                    <br></br>
+                    <br></br>
+                    <Button variant="contained" color="primary" onClick={handleSubmit}
+                        sx={{ ml: '60px', display: 'block', width: "200px" }}>Submit</Button>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                </Stack>
+            </ThemeProvider>
 
-            <br></br>
-            <br></br>
-            <Button variant="contained" color="primary" onClick={handleSubmit}
-            sx={{ ml: '60px', display: 'block', width: "200px" }}>Submit</Button>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-        </Stack>
-        </ThemeProvider>
-
-    </>   
+        </>
     );
 
-    }
-  export default Survey;
-  
+}
+export default Survey;
+
