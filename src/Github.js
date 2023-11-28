da11e4da5ded78692775ee8f9cb594321d6a2139
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2023-11-28 12:33:33 -0500

    some stuff

--- src/Github.js
+++ src/Github.js
@@ -7,7 +7,7 @@ function Github(){
     return(
     <CalendarHeatmap
         startDate={new Date('2022-12-25')}
-        endDate={new Date('2023-12-31')}
+        endDate={new Date()}
         values={[
             { date: '2023-01-02', count: 1 },
             { date: '2023-01-22', count: 2 },
